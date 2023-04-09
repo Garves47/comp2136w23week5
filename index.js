1ad8582989c4f2096a6dd0e7f973f6dd0a2076e6
Garves47 <124073916+Garves47@users.noreply.github.com>
2023-04-09 15:03:01 +0000

    Assignment2Start

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 const http = require("http");
 const fs = require("fs");
 const express = require("express");
+const expressSession = require("express-session");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
 const path =require("path");
-const atmPage = fs.readFileSync("index.html");
-
-
-
 const app = express();
 const PORT = 3000;
+let user = req.session.user;
 
+app.set("view engine","ejs");
 app.use(express.static("public"));
+app.use(expressSession({
+    resave: false,
+    saveUninitialized: true,
+    secret:"secret key shh no tellin"
+  }));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+  
 app.listen(PORT, ()=>{
     console.log("App listening on port #"+ PORT);
 });
 
 app.get("/", (req, res)=>{
-    res.sendFile(path.resolve(__dirname, "index.html"))
+    res.render("index");
 })
 
 app.get("/profile", (req, res)=>{
-    res.sendFile(path.resolve(__dirname, "profile.html"));
+    res.render("profile");
 })
